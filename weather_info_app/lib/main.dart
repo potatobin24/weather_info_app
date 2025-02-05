93f9768bb412b1b4860f52306ea7a274b5c9eae7
Brian <aladebrian@gmail.com>
2025-02-05 14:33:39 -0500

    wakawakabuttoncommit (not certain if working)

--- weather_info_app/lib/main.dart
+++ weather_info_app/lib/main.dart
@@ -48,3 +48,36 @@ class MyCustomForm extends StatelessWidget {
     );
   }
 }
+class ElevatedButtonExample extends StatefulWidget {
+  const ElevatedButtonExample({super.key});
+
+  @override
+  State<ElevatedButtonExample> createState() => FetchWeather();
+}
+
+class FetchWeather extends State<ElevatedButtonExample> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: null,
+            child: const Text('Disabled'),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Enabled'),
+          ),
+        ],
+      ),
+    );
+  }
+}
